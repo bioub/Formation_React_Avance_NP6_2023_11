6dabf868dae37a3c5af63a84c52a7bfcfca54b92
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-11-20 16:38:20 +0100

    Exerices

--- rappels-react/src/Select.jsx
+++ rappels-react/src/Select.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react';
+import './Select.css';
+import { useEffect, useRef, useState } from 'react';
 
 function Select({ value, onChange, items }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const hostRef = useRef(null);
+
+  useEffect(() => {
+    const callback = (event) => {
+      if (!hostRef.current?.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    }
+
+    window.addEventListener('click', callback);
+    return () => {
+      window.removeEventListener('click', callback);
+    }
+  }, []);
+
   return (
-    <div className="Select">
-      <div className="selected" onClick={() => setMenuOpen(!menuOpen)}>{value}</div>
+    <div className="Select" ref={hostRef}>
+      <div className="selected" onClick={() => setMenuOpen(!menuOpen)}>
+        {value}
+      </div>
       {menuOpen && (
         <div className="menu">
           {items.map((item) => (
-            <div key={item}>{item}</div>
+            <div key={item} onClick={() => onChange(item)}>
+              {item}
+            </div>
           ))}
         </div>
       )}
